c972ee44d2fd35c3618bcdff14eb960f0c4ef9ef
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-10-02 13:15:40 -0700

    topics can be selected and affect which questions come to view. experimenting with how to store data.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import questionsData from './data/questionsData';
+import questions_UI from './data/questionsData';
+const allQuestions = [...questionsData, ...questions_UI]
 
 function App() {
-  const [questions, setQuestions] = useState(questionsData);
+  const [questions, setQuestions] = useState(allQuestions);
   const [topic, setTopic] = useState()
+
+  useEffect(() => {
+    console.log("useEffect: ", topic)
+    console.log("useEffect: ", allQuestions)
+  }, [topic])
+
+  const handleChange = (e) => {
+    setTopic(e.target.value);
+  }
 
   return (
     <div className="App">
         <h1>CoderQs</h1>
         <div>
           {questions.map(q => {
-            if(q.topic === "User Interface"){
+            if(q.topic === `${topic}`){
               return(
               <div>
                 <span>{q.id} &nbsp;</span>
@@ -19,7 +30,7 @@ function App() {
               </div>
             )}
           })}
-          <select>
+          <select name="topic" value={questions.topic} onChange={handleChange}>
             <option value="">Select a Topic</option>
             <option value="User Interface">User Interface</option>
             <option value="Git">Git</option>
--- src/data/questions_UI.js
+++ src/data/questions_UI.js
@@ -1,4 +1,4 @@
-export default questionsData = [
+const questionsData = [
     {
         id: 0,
         unit: 0,
@@ -8,31 +8,33 @@ export default questionsData = [
     {
         id: 1,
         unit: 1,
-        topic: "User",
-        question: "1. If you were to describe semantic HTML to the next cohort of students, what would you say?"
+        topic: "User Interface",
+        question: "If you were to describe semantic HTML to the next cohort of students, what would you say?"
     },
     {
         id: 2,
         unit: 1,
-        topic: "User",
+        topic: "User Interface",
         question: "Name two big differences between `display: block;` and `display: inline;`"
     },
     {
         id: 3,
         unit: 1,
-        topic: "User",
+        topic: "User Interface",
         question: "What are the 4 areas of the box model?"
     },
     {
         id: 4,
         unit: 1,
-        topic: "User",
+        topic: "User Interface",
         question: "While using flexbox, what axis does the following property work on: `align-items: center`?"
     },
     {
         id: 5,
         unit: 1,
-        topic: "Gitt",
+        topic: "Git",
         question: "Explain why git is valuable to a team of developers."
     }
-]
+];
+
+export default questionsData;
