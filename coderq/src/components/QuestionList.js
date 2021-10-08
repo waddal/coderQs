da7e6a7e06063f1aec7207964b999851b31a2f7c
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-10-08 15:08:44 -0700

    questionList display formatted.

---
+++ coderq/src/components/QuestionList.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import questionsData from '../database/questionsData';
+
+const QuestionList = () => {
+    const [answer, setAnswer] = useState(false);
+
+    const showAnswer = () => {
+        setAnswer(!answer);
+    }
+
+    return (
+        <>
+            {
+            questionsData.map(q => {
+                return (
+                    <div className="question">
+                        <p>topic: {q.topic}</p>
+                        <h3 onClick={showAnswer}>q: {q.question}</h3>
+                        {
+                            answer && <h3 onClick={showAnswer}>a: {q.answer}</h3>
+                        }
+                        <hr/>
+                    </div>
+                )
+            })
+            }
+        </>
+    )
+}
+
+export default QuestionList;
---
+++ coderq/src/database/questionsData.js
@@ -0,0 +1,10 @@
+const questionsData = [
+    {
+        id: 0,
+        topic: "IDK",
+        question: "where the hell did all my work go?",
+        answer: "into a black hole",
+    }
+]
+
+export default questionsData;
