b6182b5be6f80c849c0c2a197219e65fa6fc3e20
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-10-10 18:36:14 -0700

    added more questions to database.

--- coderq/src/components/QuestionList.js
+++ coderq/src/components/QuestionList.js
@@ -13,9 +13,9 @@ const QuestionList = () => {
             {
             questionsData.map(q => {
                 return (
-                    <div className="question">
+                    <div key={q.id} className="question">
                         <p>topic: {q.topic}</p>
-                        <h3 onClick={showAnswer}>q: {q.question}</h3>
+                        <h3 onClick={showAnswer(q.id)}>q: {q.question}</h3>
                         {
                             answer && <h3 onClick={showAnswer}>a: {q.answer}</h3>
                         }
--- coderq/src/database/questionsData.js
+++ coderq/src/database/questionsData.js
@@ -4,7 +4,43 @@ const questionsData = [
         topic: "IDK",
         question: "where the hell did all my work go?",
         answer: "into a black hole",
-    }
+    },
+    {
+        id: 1,
+        topic: "HTML",
+        question: "What is semtantic HTML?",
+        answer: "",
+    },
+    {
+        id: 2,
+        topic: "CSS",
+        question: "Name two big differences between display: block; and display:inline?",
+        answer: "",
+    },
+    {
+        id: 3,
+        topic: "CSS",
+        question: "What are the 4 areas of the box model?",
+        answer: "1. Content, the inner portion. 2. Padding, what surrounds the content. 3. Border, this encases the above. 4. Margin, invisible space around the box.",
+    },
+    {
+        id: 4,
+        topic: "CSS",
+        question: "While using flexbox, what axis does `align-items: center` work on?",
+        answer: "",
+    },
+    {
+        id: 5,
+        topic: "Git",
+        question: "Why is git valuable to a team of developers?",
+        answer: "",
+    },
+    {
+        id: 6,
+        topic: "CSS",
+        question: "",
+        answer: "",
+    },
 ]
 
 export default questionsData;
